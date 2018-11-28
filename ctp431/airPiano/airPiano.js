ab13967b2471818305bf23e2571bc16c3f8077a3
Yralec <hipigmaster@gmail.com>
2018-11-28 20:51:09 +0900

    2 hands

--- ctp431/airPiano/airPiano.js
+++ ctp431/airPiano/airPiano.js
@@ -1,5 +1,15 @@
-//emitter.setMaxListeners(50)
-
+var notes = []
+var leftLineGeometries = []
+var rightLineGeometries = []
+var limits = [-0.15, 0.15]
+var leftDown = [false, false, false]
+var rightDown = [false, false, false]
+var noteOnDistance = 0.02
+var noteOffDistance = 0.04
+var currentMelodyNote = 0
+var currentMelodyFinger = 0 //0 index, 1 middle, 2 ring
+var info = document.getElementById('info')
+var melody = [1,2,3,6,7,9,10]
 // Set up plugins
 
 Leap.loop({
@@ -15,8 +25,23 @@ Leap.loop({
 			antialias: true
 		}
 	})
-	.use('proximity');
+	.use('proximity')
 
+Leap.loopController.on('frame', function(frame){
+
+    if (!frame.hands.length > 0) return;
+
+
+    var hands = frame.hands
+    for(var i = 0; i < 1; ++i){
+	    var hand = hands[0]
+	    if(!hand){return}
+		leftHand(hand)
+		var hand = hands[1]
+		if(!hand){return}
+		rightHand(hand)
+	}
+})
 
 // Set up scene
 
@@ -26,7 +51,6 @@ var renderer = Leap.loopController.plugins.boneHand.renderer;
 camera.position.set(0, 0.3, 0.6);
 
 var controls = new THREE.OrbitControls(camera);
-
 var axisHelper = new THREE.AxisHelper(0.1);
 scene.add(axisHelper);
 
@@ -37,136 +61,162 @@ var base = new THREE.Mesh(new THREE.BoxGeometry(0.1, 0.1, 0.1), new THREE.MeshPh
 base.position.set(0, 0, 0);
 base.rotateX(Math.PI * -0.5);
 
-//createScale(3)
-createScale(4)
-//createScale(5)
-
 scene.add(base);
 
-function createScale(scale){
-	var s = 69+(scale-4)*12
-	createWhitePianoNote(s)
-	createBlackPianoNote(s+1)
-	createWhitePianoNote(s+2)
-	createBlackPianoNote(s+3)
-	createWhitePianoNote(s+4)
-	createWhitePianoNote(s+5)
-	createBlackPianoNote(s+6)
-	createWhitePianoNote(s+7)
-	createBlackPianoNote(s+8)
-	createWhitePianoNote(s+9)
-	createBlackPianoNote(s+10)
-	createWhitePianoNote(s+11)
+
+function setup(){
+	var board = createColouredScale(scene)
+	for(var i = 0; i < 3; ++i){
+		leftLineGeometries[i] = new THREE.Geometry()
+		leftLineGeometries[i].vertices.push( new THREE.Vector3( 0, 0, 0 ), new THREE.Vector3( 0, 0, -10 ) );
+		addLine(leftLineGeometries[i])
+		rightLineGeometries[i] = new THREE.Geometry()
+		rightLineGeometries[i].vertices.push( new THREE.Vector3( 0, 0, 0 ), new THREE.Vector3( 0, 0, -10 ) );
+		addLine(rightLineGeometries[i])
+	}
 }
 
-function createNote(midiNote, w, h, color){
-	var planeGeo = new THREE.PlaneGeometry(w, h);
-	var material = new THREE.MeshPhongMaterial({
-		side: THREE.DoubleSide,
-		color: new THREE.Color(color)
-	});
-	var buttonMesh = new THREE.Mesh(planeGeo, material);
-	var iPlane = new InteractablePlane(buttonMesh, Leap.loopController)
-	iPlane.note_ = midiNote
-	iPlane.on('touch', function() {
-			play(this.note_)})
-		.on('release', function() {
-			release(this.note_)})
-	var squareButton = new PushButton(
-		iPlane,
-		{
-			locking: false,
-			longThrow: -0.01
-		}
-
-	)
-	return {squareButton, buttonMesh}
+function createColouredScale(base){
+	var d = limits[1] - limits[0]
+	for(var i = 0; i < 12; ++i){
+		var geometry = new THREE.PlaneGeometry(1 * d/12,0.2,12)
+		var material = new THREE.MeshBasicMaterial({color: new THREE.Color().setHSL(i/12,0.8,0.5), side: THREE.DoubleSide})
+		var plane = new THREE.Mesh(geometry, material)
+		plane.translateX(limits[0] + d*i/11)
+		base.add(plane)
+	}
+	return plane
 }
 
-function createWhitePianoNote(midiNote){
+function addLine(geo){
+	var line = new THREE.Line(geo, new THREE.LineBasicMaterial({color: 0x0000ff}))
+	scene.add(line)
+}
 
-	var {squareButton, buttonMesh} = createNote(midiNote, 0.028, 0.1, 'white')
+function positionToMidi(position){
+	var d = (limits[1] - limits[0])
+	if(position < limits[0]){
+		position = limits[0]
+	} else if(position > limits[1]){
+		position = limits[1]
+	}
+	return 60 + (position - limits[0])/d * 12
+}
 
-	var pos = getWhitePos(midiNote)
-	if(pos == -1){
-		return null
+setup()
+
+
+
+function leftHand(hand){
+	var thumbTip = new THREE.Vector3().fromArray(hand.thumb.tipPosition)
+	leftLineGeometries[0].vertices[0].fromArray(hand.indexFinger.tipPosition)
+	leftLineGeometries[0].vertices[1] = thumbTip
+	leftLineGeometries[0].verticesNeedUpdate = true
+	leftLineGeometries[1].vertices[0].fromArray(hand.middleFinger.tipPosition)
+	leftLineGeometries[1].vertices[1] = thumbTip
+	leftLineGeometries[1].verticesNeedUpdate = true
+	leftLineGeometries[2].vertices[0].fromArray(hand.ringFinger.tipPosition)
+	leftLineGeometries[2].vertices[1] = thumbTip
+	leftLineGeometries[2].verticesNeedUpdate = true
+
+	var d1 = leftLineGeometries[0].vertices[0].distanceTo(leftLineGeometries[0].vertices[1])
+	var d2 = leftLineGeometries[1].vertices[0].distanceTo(leftLineGeometries[1].vertices[1])
+	var d3 = leftLineGeometries[2].vertices[0].distanceTo(leftLineGeometries[2].vertices[1])
+
+	//console.log(thumbTip)
+	//console.log(d1+" "+d2+" "+d3)
+	var midi = positionToMidi(thumbTip.x)
+	info.innerHTML = Tone.Frequency(midi, "midi").toNote()
+	if(!leftDown[0] && d1 < noteOnDistance ){
+		playLeftIndex(midi)
+		leftDown[0] = true
+	} else if(leftDown[0] && d1 > noteOffDistance){
+		leftDown[0] = false
+	}
+	if(!leftDown[1] && d2 < noteOnDistance){
+		playLeftMiddle(midi)
+		leftDown[1] = true
+	} else if(leftDown[1] && d2 > noteOffDistance){
+		leftDown[1] = false
+	}
+	if(!leftDown[2] && d3 < noteOnDistance){
+		playLeftRing(midi)
+		leftDown[2] = true
+	} else if(leftDown[2] && d3 > noteOffDistance){
+		leftDown[2] = false
+	}
+}
+
+function playLeftIndex(midi){
+	keyDown(midi)
+}
+function playLeftMiddle(midi){
+	keyDown(midi+4)
+}
+function playLeftRing(midi){
+	keyDown(midi+7)
+}
+
+function rightHand(hand){
+	var thumbTip = new THREE.Vector3().fromArray(hand.thumb.tipPosition)
+	rightLineGeometries[0].vertices[0].fromArray(hand.indexFinger.tipPosition)
+	rightLineGeometries[0].vertices[1] = thumbTip
+	rightLineGeometries[0].verticesNeedUpdate = true
+	rightLineGeometries[1].vertices[0].fromArray(hand.middleFinger.tipPosition)
+	rightLineGeometries[1].vertices[1] = thumbTip
+	rightLineGeometries[1].verticesNeedUpdate = true
+	rightLineGeometries[2].vertices[0].fromArray(hand.ringFinger.tipPosition)
+	rightLineGeometries[2].vertices[1] = thumbTip
+	rightLineGeometries[2].verticesNeedUpdate = true
+
+	var d1 = rightLineGeometries[0].vertices[0].distanceTo(rightLineGeometries[0].vertices[1])
+	var d2 = rightLineGeometries[1].vertices[0].distanceTo(rightLineGeometries[1].vertices[1])
+	var d3 = rightLineGeometries[2].vertices[0].distanceTo(rightLineGeometries[2].vertices[1])
+
+	if(!rightDown[0] && d1 < noteOnDistance ){
+		playRight(-1,-1,-7, 0)
+		rightDown[0] = true
+	} else if(rightDown[0] && d1 > noteOffDistance){
+		rightDown[0] = false
+	}
+	if(!rightDown[1] && d2 < noteOnDistance){
+		playRight(+1,0,-1, 1)
+		rightDown[1] = true
+	} else if(rightDown[1] && d2 > noteOffDistance){
+		rightDown[1] = false
+	}
+	if(!rightDown[2] && d3 < noteOnDistance){
+		playRight(7,1,1, 2)
+		rightDown[2] = true
+	} else if(rightDown[2] && d3 > noteOffDistance){
+		rightDown[2] = false
+	}
+}
+
+function playRight(x,y,z, f){
+	if(currentMelodyFinger == 0){
+		currentMelodyNote+=x
+		keyDown(melodyToNote(currentMelodyNote))
+	} else if(currentMelodyFinger == 1){
+		currentMelodyNote+=y
+		keyDown(melodyToNote(currentMelodyNote))
 	} else{
-		buttonMesh.position.set(0.03*(pos), 0.02, 0.1);
-		squareButton.plane.resetPosition(); // resets the original position, etc to the current one
-		base.add(buttonMesh);
-		return squareButton
+		currentMelodyNote+=z
+		keyDown(melodyToNote(currentMelodyNote))
 	}
-}
-function createBlackPianoNote(midiNote){
-
-	var {squareButton, buttonMesh} = createNote(midiNote, 0.015, 0.1, 'black')
-
-	var pos = getBlackPos(midiNote)
-	if(pos == -1){
-		return
-	} else{
-		buttonMesh.position.set(-0.015 + 0.03*(pos), 0.06, 0.106);
-		squareButton.plane.resetPosition(); // resets the original position, etc to the current one
-		base.add(buttonMesh);
-		return squareButton
-	}
+	currentMelodyFinger = f
 }
 
-function getWhitePos(midiNote){
-	var x = (((midiNote - 69) % 12) +12)%12
-	var pos = 0
-	switch (x){
-		case 0:
-			pos = 0
-			break;
-		case 2:
-			pos = 1
-			break;
-		case 4:
-			pos = 2
-			break;
-		case 5:
-			pos = 3
-			break;
-		case 7:
-			pos = 4
-			break;
-		case 9:
-			pos = 5
-			break;
-		case 11:
-			pos = 6
-			break;
-		default:
-			console.error("ERR incorrect white note midi")
-			pos = -1
-			break;
-	}
-	return pos
+function melodyToNote(x){
+	return 60 + 12*Math.floor(x/7) + melody[(x%7+7)%7]
 }
-function getBlackPos(midiNote){
-	var x = (((midiNote - 69) % 12) +12)%12
-	var pos = 0
-	switch (x){
-		case 1:
-			pos = 1
-			break;
-		case 3:
-			pos = 2
-			break;
-		case 6:
-			pos = 4
-			break;
-		case 8:
-			pos = 5
-			break;
-		case 10:
-			pos = 6
-			break;
-		default:
-			console.error("ERR incorrect black note midi")
-			pos = -1
-			break;
+
+document.addEventListener('keydown', (e)=>{
+	if(e.key == 'a'){
+		playRight(-1,-1,-7, 0)
+	} else if(e.key == 's'){
+		playRight(1,0,-1, 1)
+	} else if(e.key == 'd'){
+		playRight(7,1,1, 2)
 	}
-	return pos
-}
+})
--- ctp431/airPiano/poly_synth_tonejs.js
+++ ctp431/airPiano/poly_synth_tonejs.js
@@ -1,4 +1,4 @@
-var synth = new Tone.PolySynth(48, Tone.MonoSynth).toMaster();
+var synth = new Tone.PolySynth(6, Tone.MonoSynth).toMaster();
 
 // one octave up
 synth.set("detune", 0);
@@ -67,10 +67,240 @@ function minorChordUp(midi_note_num){
 	keyUp(midi_note_num+7)
 }
 
+function pianoDown(midi_note_num){
+
+}
+
 function play(midi_note_num){
 	keyDown(midi_note_num)
 }
 
 function release(midi_note_num){
 	keyUp(midi_note_num)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function createScale(scale) {
+	var s = 69 + (scale - 4) * 12
+	createWhitePianoNote(s)
+	createBlackPianoNote(s + 1)
+	createWhitePianoNote(s + 2)
+	createBlackPianoNote(s + 3)
+	createWhitePianoNote(s + 4)
+	createWhitePianoNote(s + 5)
+	createBlackPianoNote(s + 6)
+	createWhitePianoNote(s + 7)
+	createBlackPianoNote(s + 8)
+	createWhitePianoNote(s + 9)
+	createBlackPianoNote(s + 10)
+	createWhitePianoNote(s + 11)
+}
+
+function createNote(midiNote, w, h, color) {
+
+	var planeGeo = new THREE.PlaneGeometry(w, h);
+	var material = new THREE.MeshPhongMaterial({
+		side: THREE.DoubleSide,
+		color: new THREE.Color(color)
+	});
+	var buttonMesh = new THREE.Mesh(planeGeo, material);
+	var iPlane = new InteractablePlane(buttonMesh, Leap.loopController)
+	iPlane.note_ = midiNote
+	iPlane.on('touch', function() {
+			play(this.note_)
+		})
+		.on('release', function() {
+			release(this.note_)
+		})
+	var squareButton = new PushButton(
+		iPlane, {
+			locking: false,
+			longThrow: -0.01
+		}
+
+	)
+
+	var newNote = new Note(squareButton, proximity)
+
+	notes.push(newNote)
+
+	return {
+		squareButton,
+		buttonMesh
+	}
+}
+
+function createWhitePianoNote(midiNote) {
+
+	var {
+		squareButton,
+		buttonMesh
+	} = createNote(midiNote, 0.028, 0.1, 'white')
+
+	var pos = getWhitePos(midiNote)
+	if (pos == -1) {
+		return null
+	} else {
+		buttonMesh.position.set(0.03 * (pos), 0.02, 0.1);
+		squareButton.plane.resetPosition(); // resets the original position, etc to the current one
+		base.add(buttonMesh);
+		return squareButton
+	}
+}
+
+function createBlackPianoNote(midiNote) {
+
+	var {
+		squareButton,
+		buttonMesh
+	} = createNote(midiNote, 0.015, 0.1, 'black')
+
+	var pos = getBlackPos(midiNote)
+	if (pos == -1) {
+		return
+	} else {
+		buttonMesh.position.set(-0.015 + 0.03 * (pos), 0.06, 0.106);
+		squareButton.plane.resetPosition(); // resets the original position, etc to the current one
+		base.add(buttonMesh);
+		return squareButton
+	}
+}
+
+/**
+ * Coverts 0-11 to 0-6 mapping the halfnotes from C to the corresponding white key
+ * @param  {int} midiNote
+ * @return {int} position
+ */
+function getWhitePos(midiNote) {
+	var x = (((midiNote - 69) % 12) + 12) % 12
+	var pos = 0
+	switch (x) {
+		case 0:
+			pos = 0
+			break;
+		case 2:
+			pos = 1
+			break;
+		case 4:
+			pos = 2
+			break;
+		case 5:
+			pos = 3
+			break;
+		case 7:
+			pos = 4
+			break;
+		case 9:
+			pos = 5
+			break;
+		case 11:
+			pos = 6
+			break;
+		default:
+			console.error("ERR incorrect white note midi")
+			pos = -1
+			break;
+	}
+	return pos
+}
+
+/**
+ * Coverts 0-11 to 1-6 mapping the halfnotes from C to the corresponding black key
+ * @param  {int} midiNote
+ * @return {int} position
+ */
+function getBlackPos(midiNote) {
+	var x = (((midiNote - 69) % 12) + 12) % 12
+	var pos = 0
+	switch (x) {
+		case 1:
+			pos = 1
+			break;
+		case 3:
+			pos = 2
+			break;
+		case 6:
+			pos = 4
+			break;
+		case 8:
+			pos = 5
+			break;
+		case 10:
+			pos = 6
+			break;
+		default:
+			console.error("ERR incorrect black note midi")
+			pos = -1
+			break;
+	}
+	return pos
+}
+
+function note(button, proximity) {
+	this.button = button
+	this.proximity = proximity
+}
+
+
+
+
+
+
+
+
+
+
+function playRightIndex(){
+	if(currentMelodyFinger == 0){
+		keyDown(melodyToNote(--currentMelodyNote))	//x-1
+	} else if(currentMelodyFinger == 1){
+		keyDown(melodyToNote(--currentMelodyNote))	//x-1
+	} else{
+		currentMelodyNote-=2
+		keyDown(melodyToNote(currentMelodyNote))	//x-2
+	}
+	currentMelodyFinger = 0
+	console.log(1)
+}
+function playRightMiddle(){
+	if(currentMelodyFinger == 0){
+		keyDown(melodyToNote(++currentMelodyNote))	//x+1
+	} else if(currentMelodyFinger == 1){
+		keyDown(melodyToNote(currentMelodyNote))	//x
+	} else{
+		keyDown(melodyToNote(--currentMelodyNote))	//x-1
+	}
+	currentMelodyFinger = 1
+	console.log(2)
+}
+function playRightRing(){
+	if(currentMelodyFinger == 0){
+		currentMelodyNote+=2
+		keyDown(melodyToNote(currentMelodyNote))	//x+2
+	} else if(currentMelodyFinger == 1){
+		keyDown(melodyToNote(++currentMelodyNote))	//x+1
+	} else{
+		keyDown(melodyToNote(++currentMelodyNote))	//x+1
+	}
+	currentMelodyFinger = 2
+	console.log(3)
 }
